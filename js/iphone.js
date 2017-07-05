b3b94c8ae695f937489f0e8edec5230a933f775a
IevgeniiaKiev <ievgeniia.semenova@gmail.com>
2017-07-05 16:32:56 +0300

    100% height

--- js/iphone.js
+++
@@ -1,5 +0,0 @@
-window.addEventListener("orientationchange", function() {
-  if (navigator.userAgent.match(/(iPhone|iPod|iPad)/i)) {
-    document.documentElement.innerHTML = document.documentElement.innerHTML;
-  }
-}, false);
